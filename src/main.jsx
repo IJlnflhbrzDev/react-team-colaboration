4e0afb919b3a3d8e9fc910450bcd8fe88425c2e8
IJlal Naufal Hibrizi <IJlnflhbrzDev@gmail.com>
2023-07-01 20:52:31 +0700

    Menerapkan Route Login & Register

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginPage from './pages/login';
+import RegisterPage from './pages/register';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:  <div>Hello World Home Page</div>
+  },
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
