e801eda1132c5be9534f6fe20e2cc889575db61f
IJlal Naufal Hibrizi <IJlnflhbrzDev@gmail.com>
2023-07-05 06:05:05 +0700

    Login

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,12 +1,24 @@
 import Button from "../Elements/Button/button";
-import {InputForm} from "../Elements/Input/index";
+import { InputForm } from "../Elements/Input/index";
 
 export const FormLogin = () => {
     return (
         <form>
-            <InputForm label="Email" id="email" type="email" name="email" placeholder="example@gmail.com" />
-            <InputForm label="Password" id="password" type="password" name="password" placeholder="********" />
-            <Button title="Login" /> 
+            <InputForm
+                label="Email"
+                id="email"
+                type="email"
+                name="email"
+                placeholder="example@gmail.com"
+            />
+            <InputForm
+                label="Password"
+                id="password"
+                type="password"
+                name="password"
+                placeholder="********"
+            />
+            <Button title="Login" />
         </form>
-    )
-}
+    );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import App from './App';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LoginPage from './pages/login';
-import RegisterPage from './pages/register';
-import ErrorPage from './pages/404';
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
+import ErrorPage from "./pages/404";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element:  <div>Hello World Home Page</div>,
-    errorElement: <ErrorPage />
-    
+    path: "/",
+    element: <div>Hello World</div>,
+    errorElement: <ErrorPage />,
   },
   {
-    path: '/login',
-    element: <LoginPage />
+    path: "/login",
+    element: <LoginPage />,
   },
   {
-    path: '/register',
-    element: <RegisterPage />
-  }
+    path: "/register",
+    element: <RegisterPage />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
